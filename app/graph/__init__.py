4c261bb5136796c8fe260ee959378232f2c83a22
Gaurav Jha <zda25m005@iitmz.ac.in>
2026-08-01 13:27:45 +0530

    refactor: export retrieval + schema in graph public API

--- app/graph/__init__.py
+++ app/graph/__init__.py
@@ -2,7 +2,8 @@
 
 from app.graph.client import get_graph_driver, graph_session, run_query
 from app.graph.extract import extract_graph
-from app.graph.ingest import ingest_documents, ingest_extraction
+from app.graph.ingest import ensure_graph_schema, ingest_documents, ingest_extraction
+from app.graph.retrieval import GraphContext, get_graph_context
 from app.graph.schema import Entity, GraphExtraction, Relation
 
 __all__ = [
@@ -12,6 +13,9 @@ __all__ = [
     "extract_graph",
     "ingest_documents",
     "ingest_extraction",
+    "ensure_graph_schema",
+    "get_graph_context",
+    "GraphContext",
     "Entity",
     "Relation",
     "GraphExtraction",
